4e5d30e84f143a2eb097bdb224c55e9c0ebb9edc
Tilwa Qendov <tilwa.qendov@gmail.com>
2022-03-07 09:50:17 +0100

    Export currentUniversalPlatform again

--- lib/universal_platform.dart
+++ lib/universal_platform.dart
@@ -1,2 +1,3 @@
-export 'src/universal_platform.dart' show UniversalPlatform;
+export 'src/universal_platform.dart'
+    show UniversalPlatform, currentUniversalPlatform;
 export 'src/universal_platform_type.dart' show UniversalPlatformType;
