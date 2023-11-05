9f2b3216e402fecbf07b1e405c188c8453863100
0Franky <frhaerxin@gmail.com>
2023-11-05 04:15:53 +0100

    added tv checks

--- lib/src/platform_io.dart
+++ lib/src/platform_io.dart
@@ -3,10 +3,12 @@ import '../universal_platform.dart';
 
 //Override default method, to provide .io access
 UniversalPlatformType get currentUniversalPlatform {
-  if(Platform.isWindows) return UniversalPlatformType.Windows;
-  if(Platform.isFuchsia) return UniversalPlatformType.Fuchsia;
-  if(Platform.isMacOS) return UniversalPlatformType.MacOS;
-  if(Platform.isLinux) return UniversalPlatformType.Linux;
-  if(Platform.isIOS) return UniversalPlatformType.IOS;
+  if (UniversalPlatform.isTv && Platform.isAndroid) return UniversalPlatformType.AndroidTv;
+  if (UniversalPlatform.isTv && Platform.isIOS) return UniversalPlatformType.TvOS;
+  if (Platform.isWindows) return UniversalPlatformType.Windows;
+  if (Platform.isFuchsia) return UniversalPlatformType.Fuchsia;
+  if (Platform.isMacOS) return UniversalPlatformType.MacOS;
+  if (Platform.isLinux) return UniversalPlatformType.Linux;
+  if (Platform.isIOS) return UniversalPlatformType.IOS;
   return UniversalPlatformType.Android;
 }
